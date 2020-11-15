85acf0035bcc009c64f0933ac4643b5b2727db70
Samyak S Sarnayak <samyak201@gmail.com>
2020-11-15 14:02:31 +0530

    Add basic data loading and pre-processing

---
+++ Assignment3/data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("./LBW_Dataset.csv")
+
+# TODO: better pre-processing
+df = df.dropna()
+
+columns = list(df.columns)
+columns.remove("Result")
+
+X = df[columns]
+y = df[["Result"]]
+
+# Transform to mean 0 and std 1 (works best for NNs)
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+y = y.to_numpy()
