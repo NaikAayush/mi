0776758fe8fc9968c70382f40acf4e678f29147a
NaikAayush <ayush@pesu.pes.edu>
2020-11-16 16:13:24 +0530

    new amaz processing

--- Assignment3/data.py
+++ Assignment3/data.py
@@ -5,7 +5,33 @@ from sklearn.preprocessing import StandardScaler
 df = pd.read_csv("./LBW_Dataset.csv")
 
 # TODO: better pre-processing
-df = df.dropna()
+# df = df.fillna(df.mean())
+# df = df.dropna()
+
+#Mean
+# print('Mean')
+# df = df.fillna(df.mean())
+#Median
+# print('Median')
+# df= df.fillna(df.median())
+#Zero
+# print('Zero')
+# df = df.fillna(0)
+
+#GroupBy
+grouped = df.groupby('Community')
+transformed = grouped.transform(lambda x: x.fillna(x.mean()))
+transformed['Community'] = df['Community']
+transformed = transformed[['Community', 'Age', 'Weight', 'Delivery phase', 'HB', 'IFA', 'BP','Education',	'Residence',	'Result']]
+transformed['Residence'] = df['Residence']
+transformed['Education'] = df['Education']
+
+# transformed = transformed.fillna(transformed.mode())
+transformed['Residence'].fillna(transformed['Residence'].mode()[0], inplace=True)
+transformed['Education'].fillna(transformed['Education'].mode()[0], inplace=True)
+# transformed
+
+df = transformed
 
 columns = list(df.columns)
 columns.remove("Result")
