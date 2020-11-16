34bc2d52596e548f9e6545ea3f2f962943bf3385
NaikAayush <ayush@pesu.pes.edu>
2020-11-16 22:44:38 +0530

    brr

--- Assignment3/data.py
+++ Assignment3/data.py
@@ -19,32 +19,52 @@ df = pd.read_csv("./LBW_Dataset.csv")
 # df = df.fillna(0)
 
 # GroupBy
-grouped = df.groupby("Community")
-transformed = grouped.transform(lambda x: x.fillna(x.mean()))
-transformed["Community"] = df["Community"]
-transformed = transformed[
-    [
-        "Community",
-        "Age",
-        "Weight",
-        "Delivery phase",
-        "HB",
-        "IFA",
-        "BP",
-        "Education",
-        "Residence",
-        "Result",
-    ]
-]
-transformed["Residence"] = df["Residence"]
-transformed["Education"] = df["Education"]
+# grouped = df.groupby("Community")
+# transformed = grouped.transform(lambda x: x.fillna(x.mean()))
+# transformed["Community"] = df["Community"]
+# transformed = transformed[
+#     [
+#         "Community",
+#         "Age",
+#         "Weight",
+#         "Delivery phase",
+#         "HB",
+#         "IFA",
+#         "BP",
+#         "Education",
+#         "Residence",
+#         "Result",
+#     ]
+# ]
+# transformed["Residence"] = df["Residence"]
+# transformed["Education"] = df["Education"]
 
-# transformed = transformed.fillna(transformed.mode())
-transformed["Residence"].fillna(transformed["Residence"].mode()[0], inplace=True)
-transformed["Education"].fillna(transformed["Education"].mode()[0], inplace=True)
+# # transformed = transformed.fillna(transformed.mode())
+# transformed["Residence"].fillna(transformed["Residence"].mode()[0], inplace=True)
+# transformed["Education"].fillna(transformed["Education"].mode()[0], inplace=True)
 # transformed
 
-df = transformed
+
+# df = transformed
+
+df['HB'] = df['HB'].fillna(df.groupby(['Community','Age','Weight','Delivery phase','BP'])['HB'].transform('mean'))
+df['HB'] = df['HB'].fillna(df.groupby(['Community','Age','Weight','Delivery phase'])['HB'].transform('mean'))
+df['HB'] = df['HB'].fillna(df.groupby(['Community','Age','Weight'])['HB'].transform('mean'))
+df['HB'] = df['HB'].fillna(df.groupby(['Community','Age'])['HB'].transform('mean'))
+df['HB'] = df['HB'].fillna(df.groupby('Community')['HB'].transform('mean'))
+df['BP'] = df['BP'].fillna(df.groupby(['Community','Age','Weight','Delivery phase','HB'])['BP'].transform('mean'))
+df['BP'] = df['BP'].fillna(df.groupby(['Community','Age','Weight','Delivery phase'])['BP'].transform('mean'))
+df['BP'] = df['BP'].fillna(df.groupby(['Community','Age','Weight'])['BP'].transform('mean'))
+df['BP'] = df['BP'].fillna(df.groupby(['Community','Age'])['BP'].transform('mean'))
+df['BP'] = df['BP'].fillna(df.groupby('Community')['BP'].transform('mean'))
+df['Age'] = df['Age'].fillna(df.groupby('Community')['Age'].transform('mean'))
+df['Weight'] = df['Weight'].fillna(df.groupby(['Community','Age'])['Weight'].transform('mean'))
+df['Weight'] = df['Weight'].fillna(df.groupby('Community')['Weight'].transform('mean'))
+
+df["Delivery phase"].fillna(df["Delivery phase"].mode()[0], inplace=True)
+df["Residence"].fillna(df["Residence"].mode()[0], inplace=True)
+df["Education"].fillna(df["Education"].mode()[0], inplace=True)
+
 df = pd.concat([df, df.loc[df[df.Result == 0].index.repeat(2)]])
 
 columns = list(df.columns)
--- Assignment3/train.py
+++ Assignment3/train.py
@@ -7,38 +7,52 @@ from data import X_train, X_test, y_train, y_test
 import losses
 import optim
 
-model = Sequential(
-    Dense(9, 20, ReLU, xavier_init=False),
-    Dense(20, 20, LeakyReLU, xavier_init=False),
-    Dense(20, 1, Sigmoid),
-)
-optimizer = optim.SGD(0.01, model.parameters())
-loss_fun = losses.BinaryCrossEntropy()
 
-steps = 3000
-t = tqdm(total=steps)
-for _ in range(steps):
-    optimizer.zero_grad()
-    a = model(X_train)
 
-    loss = loss_fun(y_train, a)
+for i in range(100):
+    np.random.seed(i)
+    # print("Seed: ",i)
 
-    dA = loss_fun.backward(y_train, a)
-    model.backward(dA)
-
-    optimizer.step()
-    t.set_postfix(
-        loss=loss.mean(),
-        w_mean=model.modules[0].w.data.mean(),
-        dw_mean=model.modules[0].w.grad.mean(),
+    model = Sequential(
+        Dense(9, 20, ReLU, xavier_init=False),
+        Dense(20, 20, LeakyReLU, xavier_init=False),
+        Dense(20, 1, Sigmoid),
     )
-    t.update()
-t.close()
+    optimizer = optim.SGD(0.01, model.parameters())
+    loss_fun = losses.BinaryCrossEntropy()
 
-a = model(X_train)
-y_pred = np.around(a)
-print("Train accuracy: ", sum(y_pred == y_train) / len(y_train))
+    steps = 2000
+    # t = tqdm(total=steps)
+    for _ in range(steps):
+        optimizer.zero_grad()
+        a = model(X_train)
 
-a = model(X_test)
-y_pred = np.around(a)
-print("Test accuracy: ", sum(y_pred == y_test) / len(y_test))
+        loss = loss_fun(y_train, a)
+
+        dA = loss_fun.backward(y_train, a)
+        model.backward(dA)
+
+        optimizer.step()
+        # t.set_postfix(
+        #     loss=loss.mean(),
+        #     w_mean=model.modules[0].w.data.mean(),
+        #     dw_mean=model.modules[0].w.grad.mean(),
+        # )
+        # t.update()
+    # t.close()
+
+    a = model(X_train)
+    y_pred = np.around(a)
+    tr_a = sum(y_pred == y_train) / len(y_train)
+    # print("Train accuracy: ", sum(y_pred == y_train) / len(y_train))
+
+    a = model(X_test)
+    y_pred = np.around(a)
+    te_a = sum(y_pred == y_test) / len(y_test)
+    # print("Test accuracy: ", sum(y_pred == y_test) / len(y_test))
+    if tr_a and te_a > 0.85:
+        print("Seed: ",i)
+        print("Train accuracy: ", tr_a)
+        print("Test accuracy: ", te_a)
+        print(i,tr_a,te_a,sep=',',file=open('brr.csv','a'))
+    
