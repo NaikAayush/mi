f27398b20a45a8d51be391ada15d1fe4bf3862a1
Samyak S Sarnayak <samyak201@gmail.com>
2020-11-17 20:42:50 +0530

    Better data processing, some fixes to get good acc

--- Assignment3/data.py
+++ Assignment3/data.py
@@ -1,47 +1,50 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("./LBW_Dataset.csv")
 
-# TODO: better pre-processing
-# df = df.fillna(df.mean())
-# df = df.dropna()
 
-#Mean
-# print('Mean')
-# df = df.fillna(df.mean())
-#Median
-# print('Median')
-# df= df.fillna(df.median())
-#Zero
-# print('Zero')
-# df = df.fillna(0)
+# NEW
+df["HB"] = df["HB"].fillna(
+    df.groupby(["Community", "Age", "BP"])["HB"].transform("mean")
+)
+df["HB"] = df["HB"].fillna(df.groupby(["Community", "Age"])["HB"].transform("mean"))
+df["HB"] = df["HB"].fillna(df.groupby("Community")["HB"].transform("mean"))
 
-#GroupBy
-grouped = df.groupby('Community')
-transformed = grouped.transform(lambda x: x.fillna(x.mean()))
-transformed['Community'] = df['Community']
-transformed = transformed[['Community', 'Age', 'Weight', 'Delivery phase', 'HB', 'IFA', 'BP','Education',	'Residence',	'Result']]
-transformed['Residence'] = df['Residence']
-transformed['Education'] = df['Education']
+df["BP"] = df["BP"].fillna(
+    df.groupby(["Community", "Age", "HB"])["BP"].transform("mean")
+)
+df["BP"] = df["BP"].fillna(df.groupby(["Community", "Age"])["BP"].transform("mean"))
+df["BP"] = df["BP"].fillna(df.groupby("Community")["BP"].transform("mean"))
 
-# transformed = transformed.fillna(transformed.mode())
-transformed['Residence'].fillna(transformed['Residence'].mode()[0], inplace=True)
-transformed['Education'].fillna(transformed['Education'].mode()[0], inplace=True)
-# transformed
+df["Age"] = df["Age"].fillna(df.groupby("Community")["Age"].transform("mean"))
 
-df = transformed
+df["Weight"] = df["Weight"].fillna(
+    df.groupby(["Community", "Age"])["Weight"].transform("mean")
+)
+df["Weight"] = df["Weight"].fillna(df.groupby("Community")["Weight"].transform("mean"))
+# NEWEND
+
+df["Delivery phase"].fillna(df["Delivery phase"].mode()[0], inplace=True)
+df["Residence"].fillna(df["Residence"].mode()[0], inplace=True)
+df["Education"].fillna(df["Education"].mode()[0], inplace=True)
+# NEWEND
+df = pd.concat([df, df.loc[df[df.Result == 0].index.repeat(2)]])
 
 columns = list(df.columns)
 columns.remove("Result")
 
-X = df[columns]
-y = df[["Result"]]
+X = df[columns].to_numpy()
+y = df[["Result"]].to_numpy()
 
 # Transform to mean 0 and std 1 (works best for NNs)
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
-y = y.to_numpy()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+data = np.concatenate((X, y), axis=1)
+processed_df = pd.DataFrame(data, columns=df.columns)
+processed_df.to_csv("./processed.csv", index=False)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=2)
--- Assignment3/train.py
+++ Assignment3/train.py
@@ -1,22 +1,22 @@
 import numpy as np
-from tqdm.auto import tqdm
 
 from nn import Dense, Sequential
-from activations import ReLU, Sigmoid
+from activations import LeakyReLU, ReLU, Sigmoid
 from data import X_train, X_test, y_train, y_test
 import losses
 import optim
 
+np.random.seed(94)
+
 model = Sequential(
     Dense(9, 20, ReLU, xavier_init=True),
-    Dense(20, 20, ReLU, xavier_init=True),
+    Dense(20, 20, LeakyReLU, xavier_init=True),
     Dense(20, 1, Sigmoid),
 )
-optimizer = optim.SGD(0.3, model.parameters(), l2_lambda=0.1, beta1=0.9)
+optimizer = optim.SGD(0.05, model.parameters(), l2_lambda=0.01)
 loss_fun = losses.BinaryCrossEntropy()
 
-steps = 200
-t = tqdm(total=steps)
+steps = 1000
 for _ in range(steps):
     optimizer.zero_grad()
     a = model(X_train)
@@ -27,18 +27,14 @@ for _ in range(steps):
     model.backward(dA)
 
     optimizer.step()
-    t.set_postfix(
-        loss=loss.mean(),
-        w_mean=model.modules[0].w.data.mean(),
-        dw_mean=model.modules[0].w.grad.mean(),
-    )
-    t.update()
-t.close()
 
 a = model(X_train)
 y_pred = np.around(a)
-print("Train accuracy: ", sum(y_pred == y_train) / len(y_train))
+tr_a = sum(y_pred == y_train) / len(y_train)
 
 a = model(X_test)
 y_pred = np.around(a)
-print("Test accuracy: ", sum(y_pred == y_test) / len(y_test))
+te_a = sum(y_pred == y_test) / len(y_test)
+print("Train accuracy: ", tr_a)
+print("Test accuracy: ", te_a)
+
